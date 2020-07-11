5cd4f7fa39d8d129d05fe15ea147a1cd37531f05
Cheng Wu <johnson2006wu@gmail.com>
2020-07-11 15:24:50 -0400

    some fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { StoryserviceService } from './storyservice.service';
 import { Component, OnInit } from '@angular/core';
-import {NgxPaginationModule} from 'ngx-pagination';
-import {Http} from '@angular/http';
 import { NgxSpinnerService } from 'ngx-spinner'
 
 @Component({
@@ -10,33 +8,21 @@ import { NgxSpinnerService } from 'ngx-spinner'
    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public maxSize: number = 5;
-  public directionLinks: boolean = true;
-  public autoHide: boolean = false;
-  public responsive: boolean = true;
- 
-  config: any;
-
+   p: number = 1;
    stories= [];
    searchText;
-   constructor(private service :StoryserviceService,private spinner: NgxSpinnerService){}
 
+   constructor(private service :StoryserviceService,private spinner: NgxSpinnerService){}
    ngOnInit(){
-    this.config = {
-      itemsPerPage: 20,
-      currentPage: 1,
-      totalItems: this.stories.length
-    };
+
      this.spinner.show();
-     this.service.getStories().subscribe((data: any[])=>{
+     this.service.getStories().subscribe(
+       (data: any[])=>{
       this.spinner.hide();  
       console.log(data);
       this.stories = data;
-    })
-   
+      },
+      ()=>{this.spinner.hide();
+      })
    }
-   onPageChange(event){
-
-    this.config.currentPage = event;
-  }
 }
