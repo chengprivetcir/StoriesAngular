47f7d0f46c09e1b587697c3766aa4f07eb2283a3
Cheng Wu <johnson2006wu@gmail.com>
2020-07-11 10:04:38 -0400

    boostrap add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,33 @@ import { NgxSpinnerService } from 'ngx-spinner'
    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  
+  public maxSize: number = 5;
+  public directionLinks: boolean = true;
+  public autoHide: boolean = false;
+  public responsive: boolean = true;
+ 
+  config: any;
+
    stories= [];
    searchText;
    constructor(private service :StoryserviceService,private spinner: NgxSpinnerService){}
 
    ngOnInit(){
+    this.config = {
+      itemsPerPage: 20,
+      currentPage: 1,
+      totalItems: this.stories.length
+    };
      this.spinner.show();
      this.service.getStories().subscribe((data: any[])=>{
+      this.spinner.hide();  
       console.log(data);
       this.stories = data;
     })
-    this.spinner.hide();  
+   
    }
+   onPageChange(event){
+
+    this.config.currentPage = event;
+  }
 }
