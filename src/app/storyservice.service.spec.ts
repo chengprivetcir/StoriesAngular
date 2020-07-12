44bac4832c68b782bb2f00b277548e8eeb797932
Cheng Wu <johnson2006wu@gmail.com>
2020-07-11 21:30:57 -0400

    intergration testing

--- src/app/storyservice.service.spec.ts
+++ src/app/storyservice.service.spec.ts
@@ -1,12 +1,32 @@
 import { TestBed } from '@angular/core/testing';
-
+import {HttpClientModule} from '@angular/common/http';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { StoryserviceService } from './storyservice.service';
+import { environment } from 'src/environments/environment';
 
-xdescribe('StoryserviceService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe('StoryserviceService', () => {
+  
+  let service: StoryserviceService;
+  let httpMock: HttpTestingController;
 
-  it('should be created', () => {
-    const service: StoryserviceService = TestBed.get(StoryserviceService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+      TestBed.configureTestingModule({
+          imports: [HttpClientModule,HttpClientTestingModule],
+          providers: [StoryserviceService]
+      });
+      service = TestBed.get(StoryserviceService);
+      httpMock = TestBed.get(HttpTestingController);
+  });
+
+  it('should call http request api', () => {
+    const stories = [{title: 'a'}, {title: 'b'}];
+
+    service.getStories().subscribe(res=>{
+      expect(res).toEqual(stories);
+    })
+    const req = httpMock.expectOne(request => request.url === environment.apiUrl);
+    expect(req.request.method).toBe('GET');
+    req.flush(stories);
+    httpMock.verify();
   });
 });
--- src/app/storyservice.service.ts
+++ src/app/storyservice.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../environments/environment';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class StoryserviceService {
 
   constructor(private http:HttpClient) { 
   }
 
   getStories(){
-    console.log(environment.apiUrl);
     return this.http.get(environment.apiUrl);
   }
 }
